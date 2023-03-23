bf6f9cd46192c4907065da875d3acc134224e62e
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-03-23 23:17:20 +0100

    Updated AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RconService} from "./services/rcon.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mc-server-dashboard';
+
+  constructor(private readonly rcon: RconService) {}
+
+  ngOnInit() {
+    this.sendMessage();
+  }
+
+  sendMessage() {
+    const msg = { port: 25575, host: "localhost", password: "123456" };
+    console.log(msg);
+    this.rcon.sendMessage(JSON.stringify(['initial', msg]));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { RconService } from "./services/rcon.service";
+import { CONNECT_URL, LOGGER, SimpleNgWebSocket} from "simple-ng-websocket";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,12 @@ import { HttpClientModule } from "@angular/common/http";
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    RconService,
+    { provide: CONNECT_URL, useValue: 'ws://localhost:3000' },
+    { provide: LOGGER, useValue: (level: any, message: any) => console.log(message) },
+    SimpleNgWebSocket,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
